1519b0580f4b7270896ef93bbda86b1d08fdd0d6
jiuwo <694518723@qq.com>
2019-03-25 15:58:59 +0800

    one css error deel with

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="icons">
     <swiper>
-    <swiper-slide v-for="(page,index) of pages" :key="index">
-      <div class="icon" v-for="item of page" :key="item.id">
-        <div class="icon-img">
-          <img class="icon-img-content" :src="item.imgUrl"> 
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl"> 
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
         </div>
-        <p class="icon-desc">{{item.desc}}</p>
-        </div>
-    </swiper-slide> 
+      </swiper-slide> 
     </swiper>
   </div>
 </template>
@@ -79,35 +79,37 @@ export default {
   .icons >>> .swiper-container
     height: 0
     padding-bottom 50%
-  .icon
-  position relative
-  overflow hidden
-  float left
-  width 25%
-  height 0
-  padding-bottom 25%
-  .icon-img
-    position absolute
-    top 0
-    right 0
-    left 0
-    bottom 0.44rem
-    box-sizing: border-box
-    padding: .1rem
-    .icon-img-content
-      display: block
-      margin: 0 auto
-      height: 100%
-    .icon-desc
-    position absolute
-    left 0
-    right 0
-    bottom 0
-    line-height .44rem
-    text-align center
-    height .44rem
-    color $darkTextColor
-    ellpisis()
+  .icons
+    margin-top: .1rem  
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        right 0
+        left 0
+        bottom 0.44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        line-height .44rem
+        text-align center
+        height .44rem
+        color $darkTextColor
+        ellpisis()
 </style>
 
 
