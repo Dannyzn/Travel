572b1e3da56d8b4d1d0000cc1a50cac226c50bd1
jiuwo <694518723@qq.com>
2019-03-25 16:17:15 +0800

    not use

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -86,28 +86,28 @@ export default {
   width 25%
   height 0
   padding-bottom 25%
-  .icon-img
-    position absolute
-    top 0
-    right 0
-    left 0
-    bottom 0.44rem
-    box-sizing: border-box
-    padding: .1rem
-    .icon-img-content
-      display: block
-      margin: 0 auto
-      height: 100%
+    .icon-img
+      position absolute
+      top 0
+      right 0
+      left 0
+      bottom 0.44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
     .icon-desc
-    position absolute
-    left 0
-    right 0
-    bottom 0
-    line-height .44rem
-    text-align center
-    height .44rem
-    color $darkTextColor
-    ellpisis()
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .44rem
+      text-align center
+      height .44rem
+      color $darkTextColor
+      ellpisis()
 </style>
 
 
