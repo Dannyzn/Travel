7197e2a4175990407f571a4029f2eb70bfb2e7e1
jiuwo <694518723@qq.com>
2019-03-29 01:07:46 +0800

    add weenend

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -3,10 +3,10 @@
     <div class="recommend-title">热销推荐</div>
     <ul>
         <li class="item border-bottom" v-for="(item) of recommendList" :key="item.id">
-          <img class="item-img" src="item.imgUrl">
+          <img class="item-img" :src="item.imgUrl">
           <div class="item-info">
               <p class="item-title">{{item.title}}</p>
-              <p class="item-desc">{{item.imgUrl}}</p>
+              <p class="item-desc">{{item.desc}}</p>
               <button class="item-button">查看详情</button>
           </div>
         </li>
