1eaf03742bcf0cde014df8739a71bffc6f8dd45e
jiuwo <694518723@qq.com>
2019-04-01 17:37:40 +0800

    banner-finish

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,14 +2,19 @@
   <div>
     <div class="recommend-title">热销推荐</div>
     <ul>
-        <li class="item border-bottom" v-for="(item) of list" :key="item.id">
+        <router-link tag="li"
+          :to="'/detail/' + item.id"
+          class="item border-bottom" 
+          v-for="(item) of list" 
+          :key="item.id"
+        >
           <img class="item-img" :src="item.imgUrl">
           <div class="item-info">
               <p class="item-title">{{item.title}}</p>
               <p class="item-desc">{{item.desc}}</p>
               <button class="item-button">查看详情</button>
           </div>
-        </li>
+        </router-link>
     </ul>
   </div>
 </template>
@@ -56,5 +61,5 @@ export default {
         background #ff9300
         padding 0 .1rem
         border-radius .06rem
-        color #fff  
+        color #fff
 </style>
